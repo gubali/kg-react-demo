36b8b1192e778eb764cd92be72d467f64746db95
gubali <masif_apps@live.com>
2024-01-19 14:45:49 +0530

    event hamdlers on food items('salad, dal')

--- vite-project/src/component/Items.jsx
+++ vite-project/src/component/Items.jsx
@@ -1,12 +1,14 @@
 import styles from './Items.module.css'
 
 const Items = (prop) => {
-    console.log(prop);
-    return <>
-     <li className={`${styles['list-ab']} list-group-item`} key={prop.foodItem}>
-   <span>{prop.foodItem}</span>
-  </li>
-</>
+  console.log(prop);
+  return <>
+    <li className={`${styles['list-ab']} list-group-item`} key={prop.foodItem}>
+      <span className={`${styles.floatas}`}>{prop.foodItem}</span>
+      <button type="button" id="result"
+             className={`${styles['btn']} btn btn-info`} onClick={()=>alert('Click event:'+ " " + prop.foodItem)}>Buy</button>
+    </li>
+  </>
 }
 //478197608912
 export default Items;
