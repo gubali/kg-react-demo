e2442aae59e2287f1761e778df87f23aa24ed8ef
gubali <masif_apps@live.com>
2024-01-21 19:09:03 +0530

    fofitems

--- vite-project/src/component/Items.jsx
+++ vite-project/src/component/Items.jsx
@@ -2,13 +2,13 @@ import styles from './Items.module.css'
 
 const Items = (prop) => {
   const getFoodItems = (prop)=>{
-   alert(prop.foodItem);
+   alert(prop)
   }
   return <>
     <li className={`${styles['list-ab']} list-group-item`} key={prop.foodItem}>
       <span className={`${styles.floatas}`}>{prop.foodItem}</span>
       <button type="button" id="result"
-             className={`${styles['btn']} btn btn-info`} onClick={getFoodItems}>Buy</button>
+             className={`${styles['btn']} btn btn-info`} onClick={()=>getFoodItems(prop.foodItem)}>Buy</button>
     </li>
   </>
 }
