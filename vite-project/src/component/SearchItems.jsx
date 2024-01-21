2a77a9d2a0427b93a1249cec11c5319b78627b6d
gubali <masif_apps@live.com>
2024-01-21 18:27:20 +0530

    input box on change

--- vite-project/src/component/SearchItems.jsx
+++ vite-project/src/component/SearchItems.jsx
@@ -1,6 +1,7 @@
 const SearchItems = ()=>{
     return <>
-      <input type="text" className="form-control mb-2" placeholder="Serach items.."></input>
+      <input type="text" className="form-control mb-2"
+       placeholder="Serach items.." onChange={(e)=>alert((e.target.value))}></input>
     </>
 }
 
