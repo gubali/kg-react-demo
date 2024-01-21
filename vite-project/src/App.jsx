3d01766ec43893efb836f0fb0b922040d0887955
gubali <masif_apps@live.com>
2024-01-21 18:20:32 +0530

    food items search items

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,6 +7,7 @@ import FoodItems from "./component/FoodItems";
 import Header from "./component/Header";
 import MiniCalculator from "./component/MiniCalculator";
 import Container from "./component/Container";
+import SearchItems from './component/SearchItems'
 function App() {
   let foodItems = ["Dal", "Pulse", "Green Pea", "Salad"];
   const heading = "Welcome to React App";
@@ -20,6 +21,7 @@ function App() {
       <RandomKey></RandomKey>
       <Calculator></Calculator>
       <CurrentTime></CurrentTime>
+      <SearchItems></SearchItems>
       <FoodItems items={foodItems}></FoodItems>
       <MiniCalculator></MiniCalculator>
       <hr />
---
+++ vite-project/src/component/SearchItems.jsx
@@ -0,0 +1,7 @@
+const SearchItems = ()=>{
+    return <>
+      <input type="text" className="form-control mb-2" placeholder="Serach items.."></input>
+    </>
+}
+
+export default SearchItems;
