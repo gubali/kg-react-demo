d151076e1d82a236b6ffa897a365b00e4c23bf5e
gubali <masif_apps@live.com>
2024-01-22 12:27:15 +0530

    child to parent

--- vite-project/src/component/FoodItems.jsx
+++ vite-project/src/component/FoodItems.jsx
@@ -2,12 +2,12 @@ import Items from "./Items";
 const FoodItems = ({items}) => {
   // let foodItems = ["Dal", "Pulse", "Green Pea", "Salad"];
   const getFoodItems = (items)=>{
-    alert("Clicked" + items)
+    alert("Clicked: " + items)
    }
   return (
     <ul className="list-group col-lg-6">
       {items.map((item) => (
-       <Items foodItem={item} key={item} handleBuyEvent={()=> getFoodItems()}></Items>
+       <Items foodItem={item} key={item} handleBuyEvent={()=> getFoodItems(item)}></Items>
       ))}
     </ul>
   );
