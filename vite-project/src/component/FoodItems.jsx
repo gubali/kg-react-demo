479b3055605489ec415c56e3e4dcce406fd7d783
gubali <masif_apps@live.com>
2024-01-23 15:48:53 +0530

    chnage bgcolor of food items list on click of any buy button-foodlist

--- vite-project/src/component/FoodItems.jsx
+++ vite-project/src/component/FoodItems.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Items from "./Items";
 const FoodItems = ({items}) => {
   // let foodItems = ["Dal", "Pulse", "Green Pea", "Salad"];
+  let [sateActiveitems, setActiveitems]= useState([]);
   const getFoodItems = (items)=>{
     
     alert("Clicked: " + items)
@@ -8,7 +10,7 @@ const FoodItems = ({items}) => {
   return (
     <ul className="list-group col-lg-6">
       {items.map((item) => (
-       <Items foodItem={item} key={item}  handleBuyEvent={()=> getFoodItems(item)}></Items>
+       <Items foodItem={item} key={item} bool={sateActiveitems.includes(item)} handleBuyEvent={()=> getFoodItems(item)}></Items>
       ))}
     </ul>
   );
--- vite-project/src/component/Items.jsx
+++ vite-project/src/component/Items.jsx
@@ -1,13 +1,13 @@
 import styles from './Items.module.css'
 
-const Items = ({foodItem,active, handleBuyEvent}) => {
+const Items = ({foodItem,bool, handleBuyEvent}) => {
   let as = true;
   // const getFoodItems = (foodItem)=>{
   //  alert("Clicked" + foodItem)
   // }
   return (
     <ul className=' list-group'>
-    <li className={`${styles['list-ab']} list-group-item active mb-1 ${active && onClickBgColor}`} key={foodItem}>
+    <li className={`${styles['list-ab']} list-group-item  mb-1 ${bool && active}`} key={foodItem}>
       <span className={`${styles.floatas}` } >{foodItem}</span>
       <button type="button" id="result"
              className={`${styles['btn']} btn btn-info`} onClick={handleBuyEvent}>Buy</button>
