fc9ddaab3af6c12135a9da794b7b3bc293ad8fda
gubali <masif_apps@live.com>
2024-01-24 12:10:08 +0530

    cal-style

--- vite-project/src/component/MiniCalculator.jsx
+++ vite-project/src/component/MiniCalculator.jsx
@@ -22,15 +22,15 @@ let MiniCalculator = () => {
     <div className="container">
       <h3 className="text-danger mb-3 mt-3">Mobile Calculater</h3>
       <div className="row">
-        <div className="col-lg-4"></div>
-        <div className="col-lg-4">
+        <div className="col-lg-5 col-xs-5"></div>
+        <div className="col-lg-2 col-xs-2">
           <input type="text" id="result" className="form-control"></input>
           <div className="btn-container">
             {btnList.map(item => <button type="button" id="result"
-             className="btn btn-danger" key={item}>{item}</button>)}
+             className="btn btn-danger my-1 mx-1" key={item}>{item}</button>)}
           </div>
         </div>
-        <div className="col-lg-4"></div>
+        <div className="col-lg-2 col-xs-2"></div>
       </div>
     </div>
   )
