33f7bfc9b03dfaa3dddb575aad6ea6c97b16aa0b
gubali <masif_apps@live.com>
2024-01-25 16:06:36 +0530

    add to food cart UI style

--- vite-project/src/component/AddToDoList.jsx
+++ vite-project/src/component/AddToDoList.jsx
@@ -1,15 +1,15 @@
 function AddToDoList() {
   return (
       <form className="row">
-        <div className="col-2"></div>
-        <div className="col-4">
+      
+        <div className="col-lg-4 ">
           {/* <label htmlFor="staticEmail2" className="visually-hidden">
             Email
           </label> */}
           <input type="text" className="form-control"
            id="productName" placeholder="Please type productname..."></input>
         </div>
-        <div className="col-4">
+        <div className="col-lg-4">
           {/* <label htmlFor="inputPassword2" className="visually-hidden">
             Password
           </label> */}
@@ -20,8 +20,8 @@ function AddToDoList() {
             
           ></input>
         </div>
-        <div className="col-2">
-          <button type="submit" className="btn btn-primary mb-3">
+        <div className="col-lg-2">
+          <button type="submit" className="mt-2 btn btn-primary mb-3">
             Add to list
           </button>
         </div>
